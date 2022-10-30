8b7764428e4d17b1449102c023e1b64a10ea9d73
Volkov Vitaly <juice.team@mail.ru>
2022-10-30 22:15:49 +0300

    rework test_main_page

---
+++ pages/login_page.py
@@ -0,0 +1,21 @@
+import time
+from selenium.webdriver.common.by import By
+from .base_page import BasePage
+from .locators import LoginPageLocators
+
+
+class LoginPage(BasePage):
+    def should_be_login_page(self):
+        self.should_be_login_url()
+        self.should_be_login_form()
+        self.should_be_register_form()
+
+    def should_be_login_url(self):
+        login_url = self.browser.current_url  # реализуйте проверку на корректный url адрес
+        assert "/login" in login_url, "login url is incorrect"
+
+    def should_be_login_form(self):
+        assert self.is_element_present(*LoginPageLocators.LOGIN_FORM), "Login form is not presented"
+
+    def should_be_register_form(self):
+        assert self.is_element_present(*LoginPageLocators.REGISTER_FORM), "Register form is not presented"
---
+++ test_login_page.py
@@ -0,0 +1,10 @@
+from .pages.login_page import LoginPage
+from .pages.main_page import MainPage
+
+def test_guest_should_see_login_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/accounts/login/"
+    page = MainPage(browser, link)
+    page.open()
+    page.go_to_login_page()
+    login_page = LoginPage(browser, browser.current_url)
+    login_page.should_be_login_page()
